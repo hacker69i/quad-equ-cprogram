79808de9384db1a5ce28864acf22a6ab799775b5
S Prajwall Narayana <93190550+hacker69i@users.noreply.github.com>
2022-06-08 23:18:15 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,13 +1,4 @@
-//
-//  main.c
-//  qudratic equation
-//
-//  Created by S Prajwall Narayana on 22/05/22.
-//
-
-
 #include<stdio.h>
-//#include<conio.h>
 #include<math.h>
 int main()
 {
@@ -15,25 +6,22 @@ int main()
     printf("\nEnter the value of a, b and c as in quadratic equation ax2 + bx + c = 0 :");
     scanf("%f%f%f",&a,&b,&c);
     root=b*b-(4*a*c);
-if(root>0)
-{
-
-//pow() function of the math library to calculate square root of equation
+    if(root>0)
+    {
     root3=pow(root,0.5);
     root1=(-b+root3)/(2*a);
     root2=(-b-root3)/(2*a);
     printf("\nBoth roots are different and unique\nRoot 1 = %.2f\nRoot 2 %.2f",root1,root2);
-}
-else if(root<0)
-{
+        
+    }
+    else if(root<0)
+    {
     root3=pow(-root,0.5)/(2*a);
     root1=(-b)/(2*a);
     printf("\nBoth roots are imaginary\nRoot 1 = %.2f + %.2fi\nRoot 2 = %.2f -%.2fi",root1,root3,root1,root3);
-}
-else if(root==0)
-{
-    root1=(-b)/(2*a);
-    printf("\nBoth roots are equal\n Root = %.2f",root1);
-}
-//getch();
+    }
+    else if(root==0)
+    {   root1=(-b)/(2*a);
+        printf("\nBoth roots are equal\n Roots = %.2f",root1);
+    }
 }
